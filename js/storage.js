707559d56ac40e1a592c689ed6bcb59dd69a99c5
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2021-05-10 00:07:41 -0600

    Autogenerate stats if teambuilder is in storage

--- js/storage.js
+++ js/storage.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     var ls = localStorage;
     if (ls.getItem('teambuilder')) {
         $('#teambuilderImport').val(ls.getItem('teambuilder'));
+        generateStats();
     }
     // Screw IE9 users
     $('#teambuilderImport').on('input', function () {
